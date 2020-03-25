042c3b64faf1c4674a7ed596d80f1a5289b8ec33
Tejas Kannan <tkannan@berkeley.edu>
2020-03-25 12:01:57 -0500

    Move standard model parameters to constants2

--- src/models/standard_model.py
+++ src/models/standard_model.py
@@ -13,8 +13,17 @@ from dataset.dataset import Dataset, DataSeries
 from utils.hyperparameters import HyperParameters
 from utils.tfutils import pool_rnn_outputs, get_activation, tf_rnn_cell, get_rnn_state
 from utils.constants import ACCURACY, ONE_HALF, OUTPUT, INPUTS, LOSS, PREDICTION, F1_SCORE, LOGITS, NODE_REGEX_FORMAT
-from utils.testing_utils import ClassificationMetric, RegressionMetric, get_classification_metric, get_regression_metric
+from utils.constants import INPUT_SHAPE, NUM_OUTPUT_FEATURES, INPUT_SCALER, OUTPUT_SCALER, SEQ_LENGTH, DROPOUT_KEEP_RATE, MODEL
+from utils.testing_utils import ClassificationMetric, RegressionMetric, get_classification_metric, get_regression_metric, ALL_LATENCY
 from .base_model import Model
+
+
+# Layer name constants
+EMBEDDING_LAYER_NAME = 'embedding-layer'
+TRANSFORM_LAYER_NAME = 'transform-layer'
+OUTPUT_LAYER_NAME = 'output-layer'
+RNN_NAME = 'rnn'
+BIRNN_NAME = 'birnn'
 
 
 class StandardModelType(Enum):
@@ -89,11 +98,11 @@ class StandardModel(Model):
             output_scaler = StandardScaler()
             output_scaler.fit(output_samples)
 
-        self.metadata['input_scaler'] = input_scaler
-        self.metadata['output_scaler'] = output_scaler
-        self.metadata['input_shape'] = input_shape
-        self.metadata['num_output_features'] = num_output_features
-        self.metadata['seq_length'] = seq_length
+        self.metadata[INPUT_SCALER] = input_scaler
+        self.metadata[OUTPUT_SCALER] = output_scaler
+        self.metadata[INPUT_SHAPE] = input_shape
+        self.metadata[NUM_OUTPUT_FEATURES] = num_output_features
+        self.metadata[SEQ_LENGTH] = seq_length
 
     def batch_to_feed_dict(self, batch: Dict[str, List[Any]], is_train: bool) -> Dict[tf.Tensor, np.ndarray]:
         dropout = self.hypers.dropout_keep_rate if is_train else 1.0
@@ -103,21 +112,21 @@ class StandardModel(Model):
         if input_batch.shape[1] == 1:
             input_batch = np.squeeze(input_batch, axis=1)
 
-        input_shape = self.metadata['input_shape']
-        num_output_features = self.metadata['num_output_features']
+        input_shape = self.metadata[INPUT_SHAPE]
+        num_output_features = self.metadata[NUM_OUTPUT_FEATURES]
 
         feed_dict = {
             self._placeholders[INPUTS]: input_batch,
             self._placeholders[OUTPUT]: output_batch.reshape(-1, num_output_features),
-            self._placeholders['dropout_keep_rate']: dropout
+            self._placeholders[DROPOUT_KEEP_RATE]: dropout
         }
 
         return feed_dict
 
     def make_placeholders(self, is_frozen: bool = False):
-        input_features_shape = self.metadata['input_shape']
-        num_output_features = self.metadata['num_output_features']
-        seq_length = self.metadata['seq_length']
+        input_features_shape = self.metadata[INPUT_SHAPE]
+        num_output_features = self.metadata[NUM_OUTPUT_FEATURES]
+        seq_length = self.metadata[SEQ_LENGTH]
 
         input_shape = (None, seq_length) + input_features_shape
 
@@ -129,16 +138,16 @@ class StandardModel(Model):
             self._placeholders[OUTPUT] = tf.placeholder(shape=(None, num_output_features),
                                                         dtype=tf.float32,
                                                         name=OUTPUT)
-            self._placeholders['dropout_keep_rate'] = tf.placeholder(shape=(),
-                                                                     dtype=tf.float32,
-                                                                     name='dropout-keep-rate')
+            self._placeholders[DROPOUT_KEEP_RATE] = tf.placeholder(shape=(),
+                                                                   dtype=tf.float32,
+                                                                   name=DROPOUT_KEEP_RATE)
         else:
             self._placeholders[INPUTS] = tf.ones(shape=(1,) + input_shape[1:], dtype=tf.float32, name=INPUTS)
             self._placeholders[OUTPUT] = tf.ones(shape=(1, num_output_features), dtype=tf.float32, name=OUTPUT)
-            self._placeholders['dropout_keep_rate'] = tf.ones(shape=(), dtype=tf.float32, name='dropout-keep-rate')
+            self._placeholders[DROPOUT_KEEP_RATE] = tf.ones(shape=(), dtype=tf.float32, name=DROPOUT_KEEP_RATE)
 
     def make_model(self, is_train: bool):
-        with tf.variable_scope('model', reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(MODEL, reuse=tf.AUTO_REUSE):
             self._make_model(is_train)
 
     def _make_model(self, is_train: bool):
@@ -146,12 +155,12 @@ class StandardModel(Model):
         # Embed the input sequence into a [B, T, D] tensor
         input_sequence = embedding_layer(inputs=self._placeholders[INPUTS],
                                          units=self.hypers.model_params['state_size'],
-                                         dropout_keep_rate=self._placeholders['dropout_keep_rate'],
+                                         dropout_keep_rate=self._placeholders[DROPOUT_KEEP_RATE],
                                          use_conv=self.hypers.model_params['use_conv_embedding'],
                                          params=self.hypers.model_params['embedding_layer_params'],
-                                         seq_length=self.metadata['seq_length'],
-                                         input_shape=self.metadata['input_shape'],
-                                         name_prefix=f'embedding-layer')
+                                         seq_length=self.metadata[SEQ_LENGTH],
+                                         input_shape=self.metadata[INPUT_SHAPE],
+                                         name_prefix=EMBEDDING_LAYER_NAME)
 
         # Apply the transformation layer
         if self.model_type == StandardModelType.NBOW:
@@ -160,11 +169,11 @@ class StandardModel(Model):
                               output_size=self.hypers.model_params['state_size'],
                               hidden_sizes=self.hypers.model_params['mlp_hidden_units'],
                               activations=self.hypers.model_params['mlp_activation'],
-                              dropout_keep_rate=self._placeholders['dropout_keep_rate'],
+                              dropout_keep_rate=self._placeholders[DROPOUT_KEEP_RATE],
                               should_activate_final=True,
                               should_bias_final=True,
                               should_dropout_final=True,
-                              name='transform-layer')
+                              name=TRANSFORM_LAYER_NAME)
 
             aggregated = pool_sequence(transformed, pool_mode=self.hypers.model_params['pool_mode'])
         elif self.model_type == StandardModelType.CNN:
@@ -177,7 +186,9 @@ class StandardModel(Model):
                                            activation=get_activation(self.hypers.model_params['cnn_activation']),
                                            use_bias=True,
                                            kernel_initializer=tf.glorot_uniform_initializer(),
-                                           name='transform-layer')
+                                           name=TRANSFORM_LAYER_NAME)
+            # Apply dropout
+            transformed = tf.nn.dropout(transformed, keep_rate=self._placeholders[DROPOUT_KEEP_RATE])
 
             aggregated = pool_sequence(transformed, pool_mode=self.hypers.model_params['pool_mode'])
         elif self.model_type == StandardModelType.RNN:
@@ -185,15 +196,15 @@ class StandardModel(Model):
                                num_units=self.hypers.model_params['state_size'],
                                activation=self.hypers.model_params['rnn_activation'],
                                layers=self.hypers.model_params['rnn_layers'],
-                               dropout_keep_rate=self._placeholders['dropout_keep_rate'],
-                               name_prefix='transform-layer')
+                               dropout_keep_rate=self._placeholders[DROPOUT_KEEP_RATE],
+                               name_prefix=TRANSFORM_LAYER_NAME)
 
             initial_state = cell.zero_state(batch_size=tf.shape(input_sequence)[0], dtype=tf.float32)
             transformed, state = tf.nn.dynamic_rnn(cell=cell,
                                                    inputs=input_sequence,
                                                    initial_state=initial_state,
                                                    dtype=tf.float32,
-                                                   scope='rnn')
+                                                   scope=RNN_NAME)
             final_state = get_rnn_state(state)
 
             aggregated = pool_rnn_outputs(transformed, final_state, pool_mode=self.hypers.model_params['pool_mode'])
@@ -202,15 +213,15 @@ class StandardModel(Model):
                                   num_units=self.hypers.model_params['state_size'],
                                   activation=self.hypers.model_params['rnn_activation'],
                                   layers=self.hypers.model_params['rnn_layers'],
-                                  dropout_keep_rate=self._placeholders['dropout_keep_rate'],
-                                  name_prefix='transform-layer-fw')
+                                  dropout_keep_rate=self._placeholders[DROPOUT_KEEP_RATE],
+                                  name_prefix=f'{TRANSFORM_LAYER_NAME}-fw')
             
             bw_cell = tf_rnn_cell(cell_type=self.hypers.model_params['rnn_cell_type'],
                                   num_units=self.hypers.model_params['state_size'],
                                   activation=self.hypers.model_params['rnn_activation'],
                                   layers=self.hypers.model_params['rnn_layers'],
-                                  dropout_keep_rate=self._placeholders['dropout_keep_rate'],
-                                  name_prefix='transform-layer-bw')
+                                  dropout_keep_rate=self._placeholders[DROPOUT_KEEP_RATE],
+                                  name_prefix=f'{TRANSFORM_LAYER_NAME}-bw')
 
             batch_size = tf.shape(input_sequence)[0]
             fw_initial_state = fw_cell.zero_state(batch_size=batch_size, dtype=tf.float32)
@@ -222,7 +233,7 @@ class StandardModel(Model):
                                                                  initial_state_fw=fw_initial_state,
                                                                  initial_state_bw=bw_initial_state,
                                                                  dtype=tf.float32,
-                                                                 scope='birnn')
+                                                                 scope=BIRNN_NAME)
             # Concatenate forward and backward states
             transformed = tf.concat(transformed, axis=-1)
 
@@ -237,10 +248,10 @@ class StandardModel(Model):
 
         # Create the output layer
         output = mlp(inputs=aggregated,
-                     output_size=self.metadata['num_output_features'],
+                     output_size=self.metadata[NUM_OUTPUT_FEATURES],
                      hidden_sizes=self.hypers.model_params['output_hidden_units'],
                      activations=self.hypers.model_params['output_hidden_activation'],
-                     dropout_keep_rate=self._placeholders['dropout_keep_rate'],
+                     dropout_keep_rate=self._placeholders[DROPOUT_KEEP_RATE],
                      name='output-layer')
 
         if self.output_type == OutputType.CLASSIFICATION:
@@ -272,7 +283,7 @@ class StandardModel(Model):
         with self.sess.graph.as_default():
 
             # Get FLOPS for operations that are applied to each sequence element
-            seq_operations = ['transform-layer', 'rnn', 'birnn']
+            seq_operations = [TRANSFORM_LAYER_NAME, RNN_NAME, BIRNN_NAME]
             seq_operations = list(map(lambda t: NODE_REGEX_FORMAT.format(t), seq_operations))
 
             seq_options = tf.profiler.ProfileOptionBuilder(tf.profiler.ProfileOptionBuilder.float_operation()) \
@@ -283,12 +294,12 @@ class StandardModel(Model):
             if self.model_type == StandardModelType.NBOW:
                 total_flops += flops.total_float_ops
             else:
-                total_flops += flops.total_float_ops * self.metadata['seq_length']
+                total_flops += flops.total_float_ops * self.metadata[SEQ_LENGTH]
 
             # Get FLOPS for operations that are applied to the entire sequence. We include the embedding layer
             # here because it has a well-defined sequence length so Tensorflow will automatically account for 
             # the multiplier
-            single_operations = list(map(lambda t: NODE_REGEX_FORMAT.format(t), ['output-layer', 'transform-layer']))
+            single_operations = list(map(lambda t: NODE_REGEX_FORMAT.format(t), [OUTPUT_LAYER_NAME, TRANSFORM_LAYER_NAME]))
             single_options = tf.profiler.ProfileOptionBuilder(tf.profiler.ProfileOptionBuilder.float_operation()) \
                                             .with_node_names(show_name_regexes=single_operations) \
                                             .order_by('flops').build()
@@ -324,8 +335,8 @@ class StandardModel(Model):
         result: DefaultDict[str, Dict[str, float]] = defaultdict(dict)
         for metric_name in ClassificationMetric:
             metric_value = get_classification_metric(metric_name, predictions, labels, avg_latency, 1, flops)
-            result['model'][metric_name.name] = metric_value
+            result[MODEL][metric_name.name] = metric_value
 
-        result['model']['ALL_LATENCY'] = latencies[1:]
+        result[MODEL][ALL_LATENCY] = latencies[1:]
 
         return result
