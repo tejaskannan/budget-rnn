Fix issue in accuracy calculation, model testing for standard model

src/layers/output_layers.py
@@ -60,7 +60,7 @@ def compute_multi_classification_output(model_output: tf.Tensor, labels: tf.Tens
     predictions = tf.math.argmax(predicted_probs, axis=-1, output_type=labels.dtype)  # [B]
 
     # Compute the batch-wise accuracy
-    correct = tf.cast(predictions == tf.squeeze(labels, axis=-1), dtype=tf.float32)
+    correct = tf.cast(tf.equal(predictions, tf.squeeze(labels, axis=-1)), dtype=tf.float32)
     accuracy = tf.reduce_mean(correct)
 
     # TODO: Compute the 'average' F1 score for each class

src/models/standard_model.py
@@ -328,7 +328,7 @@ class StandardModel(Model):
             # Get FLOPS for operations that are applied to the entire sequence. We include the embedding layer
             # here because it has a well-defined sequence length so Tensorflow will automatically account for 
             # the multiplier
-            single_operations = list(map(lambda t: NODE_REGEX_FORMAT.format(t), [OUTPUT_LAYER_NAME, TRANSFORM_LAYER_NAME]))
+            single_operations = list(map(lambda t: NODE_REGEX_FORMAT.format(t), [OUTPUT_LAYER_NAME, EMBEDDING_LAYER_NAME]))
             single_options = tf.profiler.ProfileOptionBuilder(tf.profiler.ProfileOptionBuilder.float_operation()) \
                                             .with_node_names(show_name_regexes=single_operations) \
                                             .order_by('flops').build()
@@ -353,10 +353,11 @@ class StandardModel(Model):
             elapsed = time.time() - start
 
             labels_list.append(np.vstack(batch[OUTPUT]))
-            predictions_list.append(prediction)
+            predictions_list.append(np.vstack(prediction))
             latencies.append(elapsed)
 
         predictions = np.vstack(predictions_list)
+
         labels = np.vstack(labels_list)
         avg_latency = np.average(latencies[1:])  # Skip first due to outliers in caching
         flops = flops_dict[self.output_ops[0]]

src/utils/testing_utils.py
@@ -36,7 +36,7 @@ LOW_SATURATION = 0.1
 
 def get_classification_metric(metric_name: ClassificationMetric, model_output: np.ndarray, expected_output: np.ndarray, latency: float, level: int, flops: Union[int, float]) -> float:
     if metric_name == ClassificationMetric.ACCURACY:
-        return float(np.average(1.0 - np.abs(model_output - expected_output)))
+        return float(np.average(np.equal(model_output, expected_output).astype(float)))
     elif metric_name == ClassificationMetric.RECALL:
         return float(recall(model_output, expected_output))
     elif metric_name == ClassificationMetric.PRECISION: