11a9a1829d9e1c3daacf438fc77333453a40896d
Tejas Kannan <tkannan@linux2.cs.uchicago.edu>
2020-08-20 17:14:50 -0500

    Adding model parameters

--- src/run.sh
+++ src/run.sh
@@ -4,10 +4,10 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=/home/tkannan/Documents/ml-models/src/slurm/out/%j.%N.stdout
 #SBATCH --error=/home/tkannan/Documents/ml-models/src/slurm/out/%j.%N.stderr
-#SBATCH --partition=general
-#SBATCH --job-name=adaptive-rnn
+#SBATCH --partition=fast
+#SBATCH --job-name=adaptive-rnn-emg-phased
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=12000
 
-python train.py --data-folders datasets/har/folds_100_rounded --save-folder saved_models --params-files params/sample_params_fifth.json params/vanilla_params_fifth.json params/rnn_params.json
+python train.py --data-folders datasets/emg/folds_50 --save-folder saved_models/emg --params-files params/emg/phased_rnn
