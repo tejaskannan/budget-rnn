a3266cdcf28e11fdddedf4b16faeb699aea82927
Tejas Kannan <tkannan@berkeley.edu>
2021-05-26 10:05:43 -0500

    Update setup file for installation

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import setuptools
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-
 setuptools.setup(
         name='budget-rnn',
-        version='0.1',
-        scripts=['run.py'],
+        version='1.0',
+        scripts=['budgetrnn/scripts'],
         author='Tejas Kannan',
         author_email='tkannan@uchicago.edu',
         description='An implementation of Budget RNNs for inference under energy budgets.',
         long_description=long_description,
         url='https://github.com/tejaskannan/budget-rnn',
-        packages['src'],
-        install_requires=['tensorflow==1.14', 'numpy', 'matplotlib', 'more_itertools', 'scipy']
+        packages=['budgetrnn'],
+        install_requires=['tensorflow==2.2', 'numpy', 'matplotlib', 'more_itertools', 'scipy', 'scikit-learn==0.22'],
+        exclude_package_data={'': ['budgetrnn/data/*', 'budgetrnn/saved_models/*', 'budgetrnn/final_models/*', '.git/*']}
 )
